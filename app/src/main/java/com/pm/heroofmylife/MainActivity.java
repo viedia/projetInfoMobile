2442f8ad3c7e5beb59acc96a05b8e9900bbe9f65
Pierrick Humbert <pierrick.humbert.68@gmail.com>
2018-02-23 17:05:32 -0500

    1ere version menu add

--- app/src/main/java/com/pm/heroofmylife/MainActivity.java
+++ app/src/main/java/com/pm/heroofmylife/MainActivity.java
@@ -1,20 +1,28 @@
 package com.pm.heroofmylife;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<String> items;
     private ArrayAdapter<String> itemsAdapter;
     private ListView lvItems;
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,9 +63,35 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void onAddItem(View view) {
-        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
-        String itemText = etNewItem.getText().toString();
-        itemsAdapter.add(itemText);
-        etNewItem.setText("");
+        //EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+     //  final EditText etNewItem = new EditText(this);
+
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.item_todo);
+        dialog.setTitle("Add new task" );
+       final EditText name = (EditText) dialog.findViewById(R.id.edit_name);
+
+        Button addButton = (Button) dialog.findViewById(R.id.task_add);
+
+        Button closeButton = (Button) dialog.findViewById(R.id.task_delete);
+        // if button is clicked, close the custom dialog
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String task = String.valueOf(name.getText());
+                itemsAdapter.add(task);
+                dialog.dismiss();
+            }
+        });
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
     }
+
 }
