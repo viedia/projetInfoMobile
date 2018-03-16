72c432ef02faed33d9da79f69fd715db33548f78
Kyohei <kalops22.kikoo@gmail.com>
2018-03-16 18:10:07 -0400

    Joueurs adding
    Ajout des setters et getters dans le joueurs + modifications

--- app/src/main/java/com/pm/heroofmylife/Joueur/Joueur.java
+++ app/src/main/java/com/pm/heroofmylife/Joueur/Joueur.java
@@ -10,9 +10,11 @@ import com.pm.heroofmylife.R;
 public class Joueur {
     private String nom;
     private int level =1;
-    private int pv = PVMAX;
     private static int PVMAX = 100;
+    private int PV = PVMAX;
+
     private int exp =0;
+    private int expMax = 100;
     private int argent=0;
     private Image image;
     private Classe classe;
@@ -22,8 +24,69 @@ public class Joueur {
         this.nom = nom;
         this.image = image;
         this.classe = classe;
-
         caracteristiques = new Caracteristique[]{new Caracteristique("Intelligence"),new Caracteristique("Force"),new Caracteristique("Agilité") };
+    }
 
+
+    public int GetPv() {
+        return PV;
+    }
+
+    public void SetPv(int PV) {
+        this.PV = PV;
+        if(PV > PVMAX)
+            PV = PVMAX;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+        if(exp > expMax) //Si il a assez d'exp, le joueur passe de niveau et perd l'exp pour un niveau
+        {
+            level+= 1;
+            exp = expMax - exp;
+        }
+    }
+
+    public int getArgent() {
+        return argent;
+    }
+
+    public void setArgent(int argent) {
+        this.argent = argent;
+        if(argent < 0)
+            argent = 0;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public Classe getClasse() {
+        return classe;
+    }
+
+    public void setClasse(Classe classe) {
+        this.classe = classe;
+    }
+
+    public Caracteristique[] getCaracteristiques() {
+        return caracteristiques;
+    }
+
+    public void setCaracteristiques(Caracteristique[] caracteristiques) {
+        this.caracteristiques = caracteristiques;
     }
 }
