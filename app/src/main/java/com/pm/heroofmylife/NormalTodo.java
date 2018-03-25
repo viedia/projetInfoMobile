e4a30d242560e1e961d4f13d089af5c5f9318821
Pierrick Humbert <pierrick.humbert.68@gmail.com>
2018-03-25 16:59:11 -0400

    interface menu d'acceuil

--- app/src/main/java/com/pm/heroofmylife/NormalTodo.java
+++ app/src/main/java/com/pm/heroofmylife/NormalTodo.java
@@ -1,6 +1,7 @@
 package com.pm.heroofmylife;
 
 
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -14,7 +15,6 @@ import android.view.ViewGroup;
 
 public class NormalTodo extends Fragment {
     View view;
-
     public NormalTodo () {
 
 
